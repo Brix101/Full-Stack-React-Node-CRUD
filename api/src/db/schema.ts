216eda0783e983bc20103d876be1bc5b3a098adb
Brixter Porras <brixterporras@gmail.com>
2025-01-18 18:43:03 +0800

    feat: added initial database connection

---
+++ api/src/db/schema.ts
@@ -0,0 +1,18 @@
+import { pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+const timestamps = {
+  updated_at: timestamp(),
+  created_at: timestamp().defaultNow().notNull(),
+  deleted_at: timestamp(),
+}
+
+export const users = pgTable("users", {
+  id: uuid("id")
+    .primaryKey()
+    .defaultRandom(),
+  name: varchar("name", {length:256}),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  ...timestamps
+});
+
